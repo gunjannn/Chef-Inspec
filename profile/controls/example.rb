4a48fc7fe3b4c183d2ef036b9aa1e60122f396ca
root <root@ip-172-31-75-85.ec2.internal>
2020-07-05 21:11:51 +0000

    corrections

--- profile/controls/example.rb
+++ profile/controls/example.rb
@@ -3,45 +3,25 @@ control 'azurerm_aks_cluster_gitops-demo-aks' do
   impact 1.0
 
 describe azurerm_aks_cluster(resource_group: 'azkubernetes', name: 'gitops-demo-aks') do
-do 
   it { should exist }
   it { should be running}
   its('location') { should cmp 'eastus' }
   its('resource_group_name') { should cmp 'azkubernetes' }
-  it { should have_tags } { 'Terraform' }
-  
+  its ('tags'){ should cmp 'Terraform' }
   its('properties.dnsPrefix') { should cmp 'gitlab' }
 end
-describe azurerm_network_security_group(resource_group: 'azkubernetes', name: 'gitops-demo-aks') 
-do
+
+describe azurerm_network_security_group(resource_group: 'azkubernetes', name: 'gitops-demo-aks') do
   its('properties.agentPoolProfiles.first.name') { should cmp 'default' }
   its('properties.agentPoolProfiles.first.count') { should cmp 1 }
   its('properties.agentPoolProfiles.first.vmSize') { should cmp 'Standard_F2s_v2' }
-    its('properties.osProfile.diskSizeGB') { should cmp 30 }
- 
-  
+  its('properties.osProfile.diskSizeGB') { should cmp 30 }
 end
 
-describe azurerm_network_security_group(resource_group: 'azkubernetes', name: 'gitops-demo-aks')
-do
+describe azurerm_network_security_group(resource_group: 'azkubernetes', name: 'gitops-demo-aks') do
   its('properties.provisioningState') { should cmp 'Succeeded' }
 end  
-
-depends:
-  - name: inspec-azure
-    git: https://github.com/inspec/inspec-azure.git
-supports:
-  platform: azure
 end
 
-my_services = yaml(content: inspec.profile.file('services.yml')).params
 
-my_services.each do |s|
-  describe service['http'] do
-    it { should be_running }
-end
-
-describe port['80' do
-  it { should be_listening }
-  end
 
